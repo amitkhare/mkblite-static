a9a5840f4a2fbe7bdabaa4c82e53e991c3fd963f
Amit Kumar Khare <me.amitkhare@gmail.com>
2023-10-17 22:16:03 +0530

    init

---
+++ precache-manifest.eb31ff9139a02431e448ff4ed97506fb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2813a6f6fc78e2abd502",
+    "url": "/css/app.0a261aa7.css"
+  },
+  {
+    "revision": "8fd625a9f1fa48feae3c",
+    "url": "/css/chunk-vendors.7f541762.css"
+  },
+  {
+    "revision": "c05522505c7067b1e97910b5a67a26be",
+    "url": "/fonts/Mukta-Bold.ttf"
+  },
+  {
+    "revision": "360d03592f5cf8264748ff6cae47ea7f",
+    "url": "/fonts/Mukta-ExtraBold.ttf"
+  },
+  {
+    "revision": "fcf25a1234026a5e1a421f7ccf7e6273",
+    "url": "/fonts/Mukta-ExtraLight.ttf"
+  },
+  {
+    "revision": "d538737e0024e7b36a6ef130b8181326",
+    "url": "/fonts/Mukta-Light.ttf"
+  },
+  {
+    "revision": "bcae6ff05c822e8b7180cf8628687a11",
+    "url": "/fonts/Mukta-Medium.ttf"
+  },
+  {
+    "revision": "6f5fbe33e286ecbaee1a2933d0de25a0",
+    "url": "/fonts/Mukta-Regular.ttf"
+  },
+  {
+    "revision": "aea68bb4da4f95fb17d3c71fb9c008ea",
+    "url": "/fonts/Mukta-SemiBold.ttf"
+  },
+  {
+    "revision": "8f5d8d2764dc8a7796b3c3d2a875d004",
+    "url": "/img/mkb/icon.png"
+  },
+  {
+    "revision": "70fbfb416e098d6f98c1924c24e434a2",
+    "url": "/img/mkb/mkb-background.png"
+  },
+  {
+    "revision": "30a5045fc338bb0c7b07ad330c082a09",
+    "url": "/img/mkb/mkb-band.png"
+  },
+  {
+    "revision": "9eb4fb7f97332618e91b580a2c66f7d5",
+    "url": "/img/mkb/mkb-bug.png"
+  },
+  {
+    "revision": "3811a346bd40f3067f9a39d1d67e9e2d",
+    "url": "/img/mkb/mkb-jacket-all-no-ticker.png"
+  },
+  {
+    "revision": "8d9d4cb3184be01ae5be6e3e18eec785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2813a6f6fc78e2abd502",
+    "url": "/js/app.bad8ab6b.js"
+  },
+  {
+    "revision": "8fd625a9f1fa48feae3c",
+    "url": "/js/chunk-vendors.323f28b3.js"
+  },
+  {
+    "revision": "df8eccaf846352616734813b3bd2f5df",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
